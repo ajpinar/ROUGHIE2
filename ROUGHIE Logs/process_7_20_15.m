10ed870da2182a7ea62081514040ae0a3035ac52
Tony Pinar <ajpinar@mtu.edu>
2015-07-22 10:55:31 -0400

    updated some comments in the code

--- ROUGHIE Logs/process_7_20_15.m
+++ ROUGHIE Logs/process_7_20_15.m
@@ -2,9 +2,10 @@
 clc
 clear
 
+% 'header' is a list of strings that define what each column in 'data' is.
 [ header, data ] = loadLog('7_20_2015_LOG_modified.CSV');
 
-%%
+%% Extract each of the columns to a meaningful variable
 time =       data(:,1);
 time = (time - time(1)) / 1000; % puts time in seconds and removes bias
 pressure =   data(:,2);
@@ -19,12 +20,12 @@ rolld =      data(:,10);
 pitchd =     data(:,11);
 yawd =       data(:,12);
 
-%% plot raw pressure reading
+%% plot raw pressure reading (these are the numbers straight from the ADC)
 figure
 plot( time, pressure ); xlabel( 'Time (ms)' );
 ylabel( 'Raw ressure reading' );
 
-%% plot estimated depth reading
+%% plot estimated depth reading using Tony's magical equation
 % (0.0423062 * (getFiltAnalog(pressureSensorPin) - 102.3));
 figure
 plot( time, 0.0423062 * (pressure - 102.3) ); xlabel( 'Time (s)' );
